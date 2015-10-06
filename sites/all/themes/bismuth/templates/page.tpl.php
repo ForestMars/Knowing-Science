d3c7bcc18ed18e5ed4550bc35b0409dfee967c7e
pbhuyan <pbhuyan@zaloni.com>
2015-10-06 19:32:24 +0530

    Test commit

--- sites/all/themes/bismuth/templates/page.tpl.php
+++ sites/all/themes/bismuth/templates/page.tpl.php
@@ -40,7 +40,7 @@ global $theme;
                 <div class="row">
                     <!-- About -->
                     <div class="col-md-3 md-margin-bottom-40">
-                        <a href="index.html"><img id="logo-footer" class="footer-logo" src="<?php echo drupal_get_path('theme',$theme) . '/assets/img/kid_knowledge/kid-knowledge-logo-stars-trans.gif' ?>" alt="" width="200px"></a>
+                        <a href="index.html"><img id="logo-footer" class="footer-logo" src="<?php echo '/'.drupal_get_path('theme',$theme) . '/assets/img/kid_knowledge/kid-knowledge-logo-stars-trans.gif' ?>" alt="" width="200px"></a>
 
                     </div><!--/col-md-3-->
                     <!-- End About -->
