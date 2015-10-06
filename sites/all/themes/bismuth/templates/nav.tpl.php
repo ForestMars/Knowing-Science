9c2ab5246802058a09bd488daa9b94f872e3d0b9
pbhuyan <pbhuyan@zaloni.com>
2015-10-06 19:13:21 +0530

    Adding the code for using the theme logo

--- sites/all/themes/bismuth/templates/nav.tpl.php
+++ sites/all/themes/bismuth/templates/nav.tpl.php
@@ -4,9 +4,18 @@
     <div class="header">
         <div class="container">
             <!-- Logo -->
-            <a class="logo" href="/">
-                <img src="<?php echo drupal_get_path('theme', variable_get('theme_default', NULL)).'/assets/img/Knowing_Science_Logo_Blue_Horizon.gif' ?>" alt="Knowing Science Logo" height="60px">
-            </a>
+<!--            <a class="logo" href="/">
+                <img src="<?php //echo drupal_get_path('theme', variable_get('theme_default', NULL)) .' /assets/img/Knowing_Science_Logo_Blue_Horizon.gif' ?>" alt="Knowing Science Logo" height="60px">
+            </a>-->
+            <?php if ($logo): ?>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+                    <img class="img-responsive" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+                </a>
+            <?php
+                else:
+                    print l("", "", array("attributes" => array("class" => "logo-link")));
+                endif;
+            ?>
             <!-- End Logo -->
 
             <!-- Topbar -->
