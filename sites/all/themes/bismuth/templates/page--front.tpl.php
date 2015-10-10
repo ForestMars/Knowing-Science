8fbfbfa8d220919f070f9236ff5e8fd95ad8f819
Forest Mars <forestmars+github@gmail.com>
2015-10-10 19:58:40 -0400

    font size

--- sites/all/themes/bismuth/templates/page--front.tpl.php
+++ sites/all/themes/bismuth/templates/page--front.tpl.php
@@ -470,11 +470,13 @@ $icon_twitter = <?php echo path_to_theme() . '//images/twitter.png';
         <div class="intro-text-1 light">
 <div class="container">
                   <div class="row">
-                    <div class="col-sm-4">
+                    <div class="col-sm-2"></div>
+
+                    <div class="col-sm-2">
                       <img src="<?php echo path_to_theme() . '/assets/img/main/texas.png' ?>" alt="" height="150">
                     </div>
                     
-                    <div class="col-sm-8">
+                    <div class="col-sm-7">
                       <h4 class="animated slideInDown">Texas adopts Knowing Science Curriculum</h4>
                       <p>The Texas State Board of Education adopted instructional materials under Proclamation 2014 at their November 2013 meeting. Proclamation 2014 includes Knowing Science K­2 Teachers Manuals, STEM kits, and Literacy Books..
                       </p>                   
